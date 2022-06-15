651c193790a751d7bd18df04edab8e2f4c6dc6c1
B3rrete <asier.berreteaga@alumni.mondragon.edu>
2022-06-15 20:37:03 +0200

    Yokese

--- src/main.c
+++ src/main.c
@@ -5,23 +5,27 @@
 
 int main(void)
 {
+
+	
 	if (kotxea_urruneko_kontrola() == false)
 	{
 		ERROREA("Ezin izan da urruneko kontrola hasi.");
 		return EXIT_FAILURE;
 	}
 
+	
 	// if (kotxea_marra_jarraitu() == false)
 	// {
 	// 	ERROREA("Ezin izan da marra jarraitu.");
 	// 	return EXIT_FAILURE;
 	// }
 
+
 	// if (kotxea_mapa() == false)
 	// {
 	// 	ERROREA("Ezin izan da marra jarraitu.");
 	// 	return EXIT_FAILURE;
 	// }
-
+	
 	return EXIT_SUCCESS;
 }
