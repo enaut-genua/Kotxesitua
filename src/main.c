d9a29441dca21177b3769f7b1174c843278ba464
Eñaut Genua Prieto <enaut.genua@alumni.mondragon.edu>
2022-06-15 19:53:48 +0200

    asdf

--- src/main.c
+++ src/main.c
@@ -5,11 +5,11 @@
 
 int main(void)
 {
-	// if (kotxea_urruneko_kontrola() == false)
-	// {
-	// 	ERROREA("Ezin izan da urruneko kontrola hasi.");
-	// 	return EXIT_FAILURE;
-	// }
+	if (kotxea_urruneko_kontrola() == false)
+	{
+		ERROREA("Ezin izan da urruneko kontrola hasi.");
+		return EXIT_FAILURE;
+	}
 
 	// if (kotxea_marra_jarraitu() == false)
 	// {
@@ -17,11 +17,11 @@ int main(void)
 	// 	return EXIT_FAILURE;
 	// }
 
-	if (kotxea_mapa() == false)
-	{
-		ERROREA("Ezin izan da marra jarraitu.");
-		return EXIT_FAILURE;
-	}
+	// if (kotxea_mapa() == false)
+	// {
+	// 	ERROREA("Ezin izan da marra jarraitu.");
+	// 	return EXIT_FAILURE;
+	// }
 
 	return EXIT_SUCCESS;
 }
